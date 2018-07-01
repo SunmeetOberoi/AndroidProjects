5e253403f5a0f00404e16f00c628bd055d705b0d
SunmeetOberoi <sunmeet.oberoi@gmail.com>
2018-07-01 19:54:26 +0530

    Added Model

--- app/src/main/java/com/learnandroid/techreporter/activities/MainActivity.java
+++ app/src/main/java/com/learnandroid/techreporter/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.learnandroid.techreporter;
+package com.learnandroid.techreporter.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.learnandroid.techreporter.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/learnandroid/techreporter/models/News.java
@@ -0,0 +1,13 @@
+package com.learnandroid.techreporter.models;
+
+import java.util.Date;
+
+public class News {
+
+    String author;
+    String title;
+    String Description;
+    String path_to_news;
+    String path_to_image;
+    Date date;
+}
