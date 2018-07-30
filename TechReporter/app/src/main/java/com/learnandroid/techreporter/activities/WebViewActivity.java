84370d6b736c46e13da17354f013b96e42360dbc
SunmeetOberoi <sunmeet.oberoi@gmail.com>
2018-07-30 18:40:06 +0530

    Added Progress Bar for WebView

--- TechReporter/app/src/main/java/com/learnandroid/techreporter/activities/WebViewActivity.java
+++ TechReporter/app/src/main/java/com/learnandroid/techreporter/activities/WebViewActivity.java
@@ -2,8 +2,10 @@ package com.learnandroid.techreporter.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import com.learnandroid.techreporter.R;
 
@@ -14,11 +16,25 @@ public class WebViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
 
+        final ProgressBar loadPage = (ProgressBar)findViewById(R.id.pbLoadNews);
+        //Start ProgressBar
+        loadPage.setVisibility(View.VISIBLE);
+
         WebView webView = (WebView) findViewById(R.id.wvWebView);
         String URL = getIntent().getStringExtra("URL");
         if(URL.equals("https://newsapi.org/"))
             webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
+
+        webView.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                //Stop ProgressBar
+                loadPage.setVisibility(View.GONE);
+            }
+        });
+
         webView.loadUrl(URL);
     }
 }
