49a481cacbd2055f7f3a8c0c9ffaa2bc9653b7d7
SunmeetOberoi <sunmeet.oberoi@gmail.com>
2018-12-21 17:49:55 +0530

    log out functionality

--- ChatApplicationFirebase/app/src/main/java/com/learnandroid/chatapplication/ContactsActivity.java
+++ ChatApplicationFirebase/app/src/main/java/com/learnandroid/chatapplication/ContactsActivity.java
@@ -1,6 +1,7 @@
 package com.learnandroid.chatapplication;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
@@ -11,6 +12,8 @@ import android.support.v7.widget.Toolbar;
 import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -25,6 +28,8 @@ import static com.learnandroid.chatapplication.ApplicationClass.mAuth;
 
 public class ContactsActivity extends AppCompatActivity {
 
+    DatabaseReference database;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,7 +39,7 @@ public class ContactsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setTitle(mAuth.getCurrentUser().getEmail().split("@")[0]);
 
-        final DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        database = FirebaseDatabase.getInstance().getReference();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -51,7 +56,7 @@ public class ContactsActivity extends AppCompatActivity {
                             public void onClick(DialogInterface dialog, int which) {
                                 final String id = etFriendsEmail.getText().toString();
                                 if(!id.isEmpty()){
-                                    database.child("Users").addValueEventListener(new ValueEventListener() {
+                                    database.child("Users").addListenerForSingleValueEvent(new ValueEventListener() {
                                         @Override
                                         public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 //                                            for(DataSnapshot ds : dataSnapshot.getChildren()) {
@@ -74,8 +79,25 @@ public class ContactsActivity extends AppCompatActivity {
                         .show();
             }
         });
-
-
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.contacts_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.actionLogOut : database.child("Users").child(mAuth.getCurrentUser().getEmail()
+                                     .replace('.',',')).child("Status").setValue("Offline");
+                                     mAuth.signOut();
+                                     startActivity(new Intent(this, LoginActivity.class));
+                                     this.finish();
+                                     break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
